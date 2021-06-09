8707c1ed4715b6ad777a352c2b1d3e9875789d31
taii0401 <taii0401@gmail.com>
2021-06-09 21:57:35 +0800

    [mod][url]修改連結

--- user/urls.py
+++ user/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login),
-    path('logout/', views.logout),
-    path('user_data/<str:action_type>/', views.user_data), #新增、編輯帳號
-    path('user_forget/', views.user_forget),
+    path('login/', views.login),                                #登入
+    path('logout/', views.logout),                              #登出
+    path('user_forget/', views.user_forget),                    #忘記密碼
+    path('user_data/<str:action_type>/', views.user_data),      #新增、編輯帳號
+    
 
-    path('ajax_user_data/', views.ajax_user_data), #檢查新增、編輯帳號
-    path('ajax_user_account/', views.ajax_user_account), #檢查帳號是否已存在
-    path('ajax_user_forget/', views.ajax_user_forget), #檢查忘記密碼資料
+    path('ajax_user_data/', views.ajax_user_data),              #檢查新增、編輯帳號
+    path('ajax_user_exist/', views.ajax_user_exist),            #檢查帳號是否已存在
+    path('ajax_user_forget/', views.ajax_user_forget),          #檢查忘記密碼資料
 ]
