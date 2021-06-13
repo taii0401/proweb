34b6bb7aaf076bb94d56488ea73690eff59f4298
taii0401 <taii0401@gmail.com>
2021-06-13 22:32:35 +0800

    [mod][models]加上檔案資料表

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 #使用者資料表
 class proweb_user(models.Model):
-    uuid = models.UUIDField(default="",null=False)
+    uuid = models.CharField(max_length=50,default="",null=False)
     user_id = models.IntegerField(default=0,null=False,unique=True)
     short_link = models.CharField(max_length=100,default="",null=True)
     name = models.CharField(max_length=30,default="",null=True)
@@ -23,7 +23,7 @@ class proweb_user(models.Model):
     class Meta:
         db_table = "proweb_user" #定義資料表名稱
 
-#檔案資料表
+#上傳檔案資料表
 class proweb_file(models.Model):
     name = models.CharField(max_length=255,default="",null=True)
     path = models.CharField(max_length=255,default="",null=True)
@@ -35,3 +35,19 @@ class proweb_file(models.Model):
     
     class Meta:
         db_table = "proweb_file" #定義資料表名稱
+
+#檔案資訊資料表
+class proweb_file_data(models.Model):
+    data_id = models.IntegerField(default=0,null=False)
+    data_type = models.CharField(max_length=100,default="",null=True)
+    file_id = models.IntegerField(default=0,null=False)
+    create_by = models.IntegerField(default=0,null=False)
+    create_time = models.DateTimeField(default="",null=True)
+    modify_by = models.IntegerField(default=0,null=False)
+    modify_time = models.DateTimeField(default="",null=True)
+    
+    def __str__(self):
+        return self.data_id
+    
+    class Meta:
+        db_table = "proweb_product_file" #定義資料表名稱
