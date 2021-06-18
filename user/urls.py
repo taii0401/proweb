c0a5c74a4d80945846afece3486494f2f48ac28f
taii0401 <taii0401@gmail.com>
2021-06-19 00:08:17 +0800

    [mod][url]修改註解說明

--- user/urls.py
+++ user/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('login/', views.login),                                #登入
     path('logout/', views.logout),                              #登出
     path('user_forget/', views.user_forget),                    #忘記密碼
-    path('user_data/<str:action_type>/', views.user_data),      #新增、編輯帳號
+    path('user_data/<str:action_type>/', views.user_data),      #新增、編輯使用者
     
 
-    path('ajax_user_data/', views.ajax_user_data),              #檢查新增、編輯帳號
-    path('ajax_user_exist/', views.ajax_user_exist),            #檢查帳號是否已存在
+    path('ajax_user_data/', views.ajax_user_data),              #檢查新增、編輯、刪除使用者
+    path('ajax_user_exist/', views.ajax_user_exist),            #檢查使用者帳號是否已存在
     path('ajax_user_forget/', views.ajax_user_forget),          #檢查忘記密碼資料
 ]
