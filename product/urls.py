ef696ff877e90d1fc24b10430a24676d2f8ecc60
taii0401 <taii0401@gmail.com>
2021-06-19 00:11:33 +0800

    [mod][url]加上頁面

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product_list/', views.product_list),      #商品列表
+    path('product_list/', views.product_list),                          #商品列表
+    #path('product_data/<str:action_type>/', views.product_data),        #新增、編輯商品
+
+    path('ajax_product_data/', views.ajax_product_data),                #檢查新增、編輯、刪除商品
 ]
